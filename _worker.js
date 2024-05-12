2e59058acd9fbfd82a76e1ae8406c403c79e847e
merryf6 <124674099+merryf6@users.noreply.github.com>
2024-05-12 17:39:15 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -190,7 +190,7 @@ export default {
 
                     default:
                         // return new Response('Not found', { status: 404 });
-                        url.hostname = 'www.speedtest.net';
+                        url.hostname = 'dl.diginetplan.cloud';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
